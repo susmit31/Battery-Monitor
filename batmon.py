f202c66ab92e7069449580586dbea06ed507b6f4
susmit31 <gohanmysticdbz@gmail.com>
2022-01-21 21:04:56 +0600

    added flickering message

--- batmon.py
+++ batmon.py
@@ -1,8 +1,26 @@
-import os, time
+import os, sys, time
+from colorain import move_cursor
 
-MINUTE = 60
+def fancy_print(text):
+    N_DOTS = 4
+    textlen = len(text)
+    _ = sys.stdout.write(text)
+    print()
+    move_cursor(-1,textlen)
+    for i in range(N_DOTS):
+        time.sleep(.5)
+        _ = sys.stdout.write(".")
+        print()
+        move_cursor(-1,textlen+i+1)
+    move_cursor(0, -textlen-N_DOTS)
+    sys.stdout.write(" "*(N_DOTS+textlen))
+    move_cursor(0,-textlen-N_DOTS)
 
-MIN_CHARGE = 20
+SLEEP_TIME = 2 #sleep for 2 "units"
+UNIT = 60 #seconds
+
+opt = input("When should we alert you?\nA. 15%\nB. 20%\nC. 25%\n(Hit Enter for default 20%)\n")
+MIN_CHARGE = 20 if opt in ['', 'b', 'B'] else (15 if opt in ['a','A'] else 25)
 
 CHECK_POW = 'upower -i $(upower -e | grep BAT) | grep -E "state|perc" > batmon.txt'
 ALERT = 'spd-say "Plug In... Plug In... Plug In"'
@@ -14,7 +32,10 @@ while True:
 
     if output[1]=='discharging':
         perc = int(output[3][:-1])
-        if perc < MIN_CHARGE:
+        if perc <= MIN_CHARGE:
             os.system(ALERT)
-    
-    time.sleep(5*MINUTE)
+
+    curr_time = time.time()
+    stop_time = curr_time + SLEEP_TIME*UNIT
+    while time.time() < stop_time:
+        fancy_print("Watching")
