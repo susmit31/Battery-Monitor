c024ecd07f7b51038cf8a3e223f1c608aeb170da
susmit31 <gohanmysticdbz@gmail.com>
2022-02-18 21:38:47 +0600

    added memory usage monitoring for Julia

--- batmon.jl
+++ batmon.jl
@@ -1,3 +1,5 @@
+using PyPlot, DataFrames, CSV
+
 struct Msg
     plugout::String
     plugin::String
@@ -7,8 +9,15 @@ msg = Msg("Plug it out, you damned brat!", "Plug it in, you frickin moron!")
 struct Commands
     plugout::Cmd
     plugin::Cmd
+    battery::Cmd
+    memory::Cmd
 end
-cmds = Commands(`espeak -p 70 -s 150 $(msg.plugout)`, `espeak -p 70 -s 150 $(msg.plugin)`)
+cmds = Commands(
+    `espeak -p 70 -s 150 $(msg.plugout)`, 
+    `espeak -p 70 -s 150 $(msg.plugin)`,
+    pipeline(`upower -e`, `grep BAT`),
+    `free -m`
+)
 
 struct Battery
     state::String
@@ -16,11 +25,23 @@ struct Battery
 end
 
 while true
-    battery_dev = read(pipeline(`upower -e`, `grep BAT`), String)[1:(end-1)]
+    # Getting battery info
+    battery_dev = read(cmds.battery, String)[1:(end-1)]
     battery_info = read(pipeline(`upower -i $battery_dev`, `grep -E "state|perc"`), String)
     battery_info = filter.(x->x!="", split.(split(battery_info, "\n")," "))
     battery = Battery([battery_info[1][2],battery_info[2][2]]...)
     charge = parse(Int8, battery.power[1:(end-1)])
+
+    # Getting memory info
+    meminfo = read(cmds.memory, String) # gets the command output as a string
+    meminfo = strip(meminfo) # strips the string of leading and trailing whitespaces
+    meminfo = split(meminfo, "\n")[2] # splits the strings into lines keeping the one line we need
+    meminfo = split(meminfo, " ") # splitting the line by whitespace
+    meminfo = filter(x->x!="", meminfo)[2:end] # getting rid of empty strings made by the split
+    totalmem = parse(Int16, meminfo[1])
+    freemem = parse(Int16, meminfo[end])
+    usedmem = totalmem - freemem
+    memusg = usedmem / totalmem
 
     if battery.state == "discharging"
         if charge <= 20
