3ebed7956252528caf33a43efebb6b106784ba67
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2022-03-24 19:02:42 +0600

    store data within the code's parent directory

--- batmon.py
+++ batmon.py
@@ -68,7 +68,7 @@ UNIT = 60 #seconds
 MSG_PLUGIN = "Plug it in, you frickin moron!" #what to yell when the battery's low
 MSG_PLUGOUT = "Plug it out, you damned brat!"
 
-os.chdir(os.sys.path[0]) # the first element of the path list
+os.chdir(sys.path[0]) # the first element of the path list
 # is the directory where the script lives
 # if the script was invoked from elsewhere,
 # we'll now move into the code's directory
